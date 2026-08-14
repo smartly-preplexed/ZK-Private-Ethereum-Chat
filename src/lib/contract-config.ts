4aa5052c6d7a5a0e49b1526d9a01c534a38e2671
smartly-preplexed <147657733+smartly-preplexed@users.noreply.github.com>
2026-08-14 01:36:11 -0500

    Create contract-config.ts

---
+++ src/lib/contract-config.ts
@@ -0,0 +1,8 @@
+export const CONTRACTS = {
+  ChatRoom:     "0x9fE46736F7734CE477F66E3c2C2F8C3d6a4b2c1Ad",
+  ZKVerifier:   "0x5FbDB2315678afecb2576b52C4b8C2E6d8dA8a42",
+  MessageStore: "0xe7f1726E7734CE477F66E3c2C2F8C3d6a4b2c1Ad",
+};
+
+export const CHAIN_ID = "0x7a69"; // 31337 in hex
+}
